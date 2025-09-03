3de32f5bf3cf59b4ebf3335e1ebf78e4b848063c
Shadow <coderdaniel6853@gmail.com>
2025-09-03 06:43:32 +0300

    completed lab

--- lib/review.py
+++ lib/review.py
@@ -47,19 +47,32 @@ class Review:
         """ Insert a new row with the year, summary, and employee id values of the current Review object.
         Update object id attribute using the primary key value of new row.
         Save the object in local dictionary using table row's PK as dictionary key"""
-        pass
+        if self.id is None:
+            sql="""
+                INSERT INTO reviews(year,summary,employee_id)
+                 VALUES(?,?,?)
+         """           
+            CURSOR.execute(sql, (self.year, self.summary, self.employee_id))
+            CONN.commit()
+            self.id = CURSOR.lastrowid
+            type(self).all[self.id] = self
+        else:
+            self.update()
+            
+            pass
 
     @classmethod
     def create(cls, year, summary, employee_id):
         """ Initialize a new Review instance and save the object to the database. Return the new instance. """
+        
         pass
    
     @classmethod
     def instance_from_db(cls, row):
-        """Return an Review instance having the attribute values from the table row."""
+        """ an Review instance having the attribute values from the table row."""
+
         # Check the dictionary for  existing instance using the row's primary key
-        pass
-   
+    
 
     @classmethod
     def find_by_id(cls, id):
